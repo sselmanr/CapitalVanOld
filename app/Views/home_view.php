d24e73411d38af5e4745f46c1e5738781cb39c0c
sselmanr <salva.selman@gmail.com>
2024-03-12 14:46:18 -0300

    incorporacion de imágenes svg para íconos

--- app/Views/home_view.php
+++ app/Views/home_view.php
@@ -1,9 +1,32 @@
 <!-- CONTENT -->
-<section class="oscuro gradiente">
-    <h1>About this page</h1>
-    <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-    <p>If you would like to edit this page you will find it located at:</p>
-    <pre><code>app/Views/welcome_message.php</code></pre>
-    <p>The corresponding controller for this page can be found at:</p>
-    <pre><code>app/Controllers/Home.php</code></pre>
-</section>
+<div class="container">
+    <h1 class="pb-2">Hola, Patrick</h1>
+    <p>Viajes en vivo</p>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.4383208054032!2d-70.65110302883606!3d-33.43788514696174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c5a39d9e805d%3A0x323783368ccc1977!2sPl.%20de%20Armas%2C%20Santiago%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1710264140606!5m2!1ses-419!2scl" width="100%"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div class="row pt-4">
+        <div class="col">1</div>
+        <div class="col">2</div>
+        <div class="col">3</div>
+        <div class="col"><span class="glyphicon glyphicon-calendar"></span></div>
+    </div>
+    <div class="row">
+        <div class="col">5</div>
+        <div class="col">6</div>
+        <div class="col">7</div>
+        <div class="col">8</div>
+    </div>
+    <div class="row">
+        <div class="col">9</div>
+        <div class="col">10</div>
+        <div class="col">11</div>
+        <div class="col">12</div>
+    </div>
+    <section class="oscuro">
+        <h1>About this page</h1>
+        <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+        <p>If you would like to edit this page you will find it located at:</p>
+        <pre><code>app/Views/welcome_message.php</code></pre>
+        <p>The corresponding controller for this page can be found at:</p>
+        <pre><code>app/Controllers/Home.php</code></pre>
+    </section>
+</div>
