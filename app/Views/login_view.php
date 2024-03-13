d2fc8ce73bf8cba9b84f071dc8f1f37097d2f23a
sselmanr <salva.selman@gmail.com>
2024-03-13 12:22:15 -0300

    login a medias. prueba de autodeploy de hostinger/github

--- app/Views/login_view.php
+++ app/Views/login_view.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="light">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CapitalVan</title>
+    <meta name="description" content="Gestion de viajes de CapitalVan">
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="public/assets/css/estilos2.css" rel="stylesheet">
 </head>
 <body>
-    
+<div class="pt-5 text-white text-center">
+  <img src="public/assets/img/logoCapitalVan.png" alt="Logo de CapitalVan" width="300" class="pt-5">
+</div>
+
+<div class="container mw-xll-25 mw-xl-25 mw-lg-25 mw-sm-25">
+  <form action="">
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Nombre de usuario</label>
+      <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Usuario">
+    </div>
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">Contraseña</label>
+      <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="******">
+    </div>
+    <div class="d-flex justify-content-center justify-content-md-end">
+      <input type="submit" value="Iniciar sesión" class="btn btn-dark mt-2 mt-md-0 w-100 w-md-50">
+    </div>
+  </form>
+</div>
 </body>
 </html>
